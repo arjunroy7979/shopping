bdee6ef48c09a080630b5134b8067c535a2d4149
Arjun Kumar <93002735+arjunroy7979@users.noreply.github.com>
2023-09-10 17:28:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,13 @@ const mongoose = require('mongoose');
 const dotenv = require("dotenv").config()
 
 const app = express();
-app.use(cors())
+app.use(cors(
+    {
+        origin:["https://shopping-pearl-pi.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true
+    }
+));
 app.use(express.json({ limit: "20mb" }))
 
 const PORT = process.env.PORT || 8080
