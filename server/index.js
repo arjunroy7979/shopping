f68e3a8ebb2392224f36e44fd4f7fc4ff698e5bb
Arjun Kumar <93002735+arjunroy7979@users.noreply.github.com>
2023-09-10 17:00:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,11 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require("dotenv").config()
 
 const app = express();
-app.use(cors(
-    {
-        "*"
-    }
-))
+app.use(cors())
 app.use(express.json({ limit: "20mb" }))
 
 const PORT = process.env.PORT || 8080
